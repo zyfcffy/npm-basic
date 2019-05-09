0b4ba2177b01bd72d1e53e2cf03ffbd9098eaf30
Yan Yu <yyan@thoughtworks.com>
2019-05-09 21:43:38 +0800

    [Yan Yu] use lodash to calculate sum

--- __test__/calculator.test.js
+++ __test__/calculator.test.js
@@ -1,9 +1,15 @@
-import {plus} from "../src/calculator";
+import {plus, sum} from "../src/calculator";
 
 describe('calculator', () => {
     it('should return 3 when 1 plus 2', () => {
         const formatted = plus(1, 2);
 
         expect(formatted).toEqual(3);
+    });
+
+    it('should return 6 when sum of 1, 2, 3', () => {
+        const result = sum([1, 2, 3]);
+
+        expect(result).toEqual(6);
     });
 });
--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,13 @@
+import _ from 'lodash';
+
+const sum = (numbers) => {
+    return _.sum(numbers);
+};
+
 const plus = (number, addend) => {
     return number + addend;
 };
 
 export {
-    plus
+    plus, sum
 };
